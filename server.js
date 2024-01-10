b7cbf1761891b01456fc546dc06363b38c8eb0af
Aditya Kumar Dixit <96368151+adityadixit07@users.noreply.github.com>
2024-01-10 14:30:17 +0530

    build

--- server.js
+++ server.js
@@ -36,16 +36,16 @@ app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 
 // Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("./client/dist"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/dist"));
+}
  
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-// const staticPath = path.join(__dirname, "./client/dist");
-// app.use(express.static(staticPath));
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/dist/index.html"));
-// });
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const staticPath = path.join(__dirname, "./client/dist");
+app.use(express.static(staticPath));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
